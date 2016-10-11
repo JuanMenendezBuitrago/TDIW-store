af644b6a1eab5b01280819ccb00270f17c255eaf
Juan Menendez <juan.menendez.buitrago@gmail.com>
2016-10-11 17:18:42 +0200

    Add HTML file for order details page

---
+++ public_html/order-detail.php
@@ -0,0 +1,197 @@
+<!doctype html>
+<html class="no-js" lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>
+        	<!-- TODO: insertar título -->
+        </title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+		<link rel="icon" href="favicon.png">
+
+        <link rel="stylesheet" href="css/normalize.css">
+        <!-- <link rel="stylesheet" href="css/grid.css"> -->
+        <link rel="stylesheet" href="css/webfonts.css">
+        <link rel="stylesheet" href="css/font-awesome.css">
+        <link rel="stylesheet" href="css/common.css">
+
+		<!-- jQuery -->
+        <script src="http://code.jquery.com/jquery-1.12.4.min.js"  defer></script>
+		<!-- holder.js crea imágenes de relleno para la etapa de diseño -->
+        <script src="https://cdn.rawgit.com/imsky/holder/master/holder.js"  defer></script>
+        <!-- custom code -->
+        <script src="js/common.js"  defer></script>
+        <script src="js/form.js"  defer></script>
+
+    </head>
+    <body class="backend list">
+    	<header>
+	    	<nav>
+	    		<div class="container">
+	    			<ul class="nav">
+	    				<li><a href="#"><i class="fa fa-home"></i>inici</a></li>
+	    				<li class="dropdown-toggle"><a href="ayuda.html">productes<i class="caret"></i></a>
+							<ul class="dropdown">
+								<li><a href="#">foo</a></li>
+								<li><a href="#">bar</a></li>
+								<li><a href="#">baz</a></li>
+							</ul>
+						</li>
+	    			</ul>
+	    			<ul class="nav login">
+	    				<li><a href="#">registra't</a></li>
+	    				<li class="dropdown-toggle"><a href="#"><i class="fa fa-user" aria-hidden="true"></i>entra<i class="caret"></i></a>
+							<ul class="dropdown">
+								<form action="">
+									<div class="form-group">
+										<input type="" class="form-control" placeholder="usuari">
+									</div>
+									<div class="form-group">
+										<input type="" class="form-control" placeholder="contrasenya">
+									</div>
+									<div class="form-group">
+										<button class="button">entra</button>
+									</div>
+								</form>
+							</ul>
+	    				</li>
+	    				<li><a href="#"><i class="fa fa-shopping-cart" aria-hidden="true"><span class="bubble">10</span></i>cistell</a></li>
+	    			</ul>
+	    		</div>
+	    	</nav>
+    	</header>
+    	<main>
+	    	<div class="container order">
+	    		<div class="row">
+	    			<div class="col-12">
+	    				<h1>comanda</h1>
+	    			</div class="col-12">
+	    		</div>
+	    		<div class="row">
+	    			<div class="col-6">
+		    			<div class="form-group">
+		    				<label for="">Nom</label> Lorem ipsum.
+		    			</div>
+		    			<div class="form-group">
+		    				<label for="">Email</label> email@email.com
+		    			</div>
+		    			<div class="form-group">
+		    				<label for="">Adreça</label> Lorem ipsum dolor sit 10
+		    			</div>
+		    			<div class="form-group">
+		    				<label for="">Població</label> Barcelona
+		    			</div>
+		    			<div class="form-group">
+		    				<label for="">Codi postal</label> 08203
+		    			</div>
+	    			</div>
+	    			<div class="col-6">
+	    				<div class="form-group">
+	    					<label for="">ID</label>
+	    					XXXXXXXXX
+	    				</div>
+	    				<div class="form-group">
+	    					<label for="">Data</label>
+	    					12/12/2016 24:00:00
+	    				</div>
+	    				<div class="form-group">
+	    					<label for="">Targeta bancaria</label>
+	    					1234 1234 1234 1234
+	    				</div>
+	    			</div>
+	    		</div>
+	    		<div class="row table-row table-header">
+	    			<div class="col-12">
+	    				<div class="item row">
+	    					<div class="col-4">producte</div>
+	    					<div class="col-2">quantitat</div>
+	    					<div class="col-2">preu/unitat</div>
+	    					<div class="col-2">preu</div>
+	    					<div class="col-2"></div>
+	    				</div>
+	    			</div>
+	    		</div>
+	    		<div class="row table-row">
+	    			<div class="col-12">
+	    				<div class="item row">
+	    					<div class="col-4">Lorem ipsum dolor</div>
+	    					<div class="col-2"><i class="fa fa-minus"></i>3<i class="fa fa-plus"></i></div>
+	    					<div class="col-2">0.50€</div>
+	    					<div class="col-2">1.50€</div>
+	    					<div class="col-2 actions"></i><i class="fa fa-trash-o"></i></div>
+	    				</div>
+	    			</div>
+	    		</div>
+	    		<div class="row table-row">
+	    			<div class="col-12">
+	    				<div class="item row">
+	    					<div class="col-4">Lorem ipsum dolor.</div>
+	    					<div class="col-2"><i class="fa fa-minus"></i>3<i class="fa fa-plus"></i></div>
+	    					<div class="col-2">0.50€</div>
+	    					<div class="col-2">1.50€</div>
+	    					<div class="col-2 actions"></i><i class="fa fa-trash-o"></i></div>
+	    				</div>
+	    			</div>
+	    		</div>
+	    		<div class="row table-row">
+	    			<div class="col-12">
+	    				<div class="item row">
+	    					<div class="col-4">Lorem ipsum dolor.</div>
+	    					<div class="col-2"><i class="fa fa-minus"></i>3<i class="fa fa-plus"></i></div>
+	    					<div class="col-2">0.50€</div>
+	    					<div class="col-2">1.50€</div>
+	    					<div class="col-2 actions"></i><i class="fa fa-trash-o"></i></div>
+	    				</div>
+	    			</div>
+	    		</div>
+	    		<div class="row table-row">
+	    			<div class="col-12">
+	    				<div class="item row">
+	    					<div class="col-4">Lorem ipsum dolor.</div>
+	    					<div class="col-2"><i class="fa fa-minus"></i>3<i class="fa fa-plus"></i></div>
+	    					<div class="col-2">0.50€</div>
+	    					<div class="col-2">1.50€</div>
+	    					<div class="col-2 actions"></i><i class="fa fa-trash-o"></i></div>
+	    				</div>
+	    			</div>
+	    		</div>
+	    		<div class="row table-row">
+	    			<div class="col-12">
+	    				<div class="item row total">
+	    					<!-- <div class="col-6">&nbsp;</div> -->
+	    					<div class="col-2 offset-6">TOTAL:</div>
+	    					<div class="col-2">10.50€</div>
+	    					<div class="col-2"></div>
+	    				</div>
+	    			</div>
+	    		</div>
+	    		<p>
+	    			
+	    				<a href="" class="button">pagar comanda</a>
+	    				<a href="" class="button pink">continuar comprant</a>
+	    		</p>
+	    	</div>
+    	</main>
+    	<footer>
+    		<div class="container">
+    			<div class="row">	
+	    			<div class="col-4 offset-8">
+						<ul>
+							<li>Contacto</li>
+							<li>Lorem Ipsum</li>
+							<li>Dolor Sit 123</li>
+							<li>Sabadell</li>
+							<li>08203 Barcelona</li>
+							<li>lorem.ipsum@lorempsum.org</li>
+							<li id="social"><a href="#"><i class="fa fa-facebook"></i></a><a href="#"><i class="fa fa-twitter"></i></a><a href="#"><i class="fa fa-instagram"></i></a></li>
+						</ul>
+	    			</div>
+	    		</div>
+    		</div>
+    	</footer>
+    </body>
+</html>
