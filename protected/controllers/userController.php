2c54f6ae6804f4fa3145ef4b20d71d6a4437f954
Juan Menendez <juan.menendez.buitrago@gmail.com>
2016-11-04 15:42:53 +0100

    Remove token validation and ad try-catch block in save method

--- protected/controllers/userController.php
+++ protected/controllers/userController.php
@@ -1,5 +1,6 @@
 <?php
 require_once(dirname(__FILE__).'/Controller.php');
+require_once(dirname(__FILE__).'/../models/User.php');
 
 class UserController extends Controller {
 
@@ -7,17 +8,36 @@ class UserController extends Controller {
 		parent::__construct('user');
 	}
 
+	/**
+	 * Render the registration form.
+	 * @return void
+	 */
 	public function actionRegister() {
+		// add id to body tag
 		$this->bodyId = "register";
+		// add script for the registration script 
 		$this->scripts[] = "register-form.js";
-		if($this->_checkToken()){
-			// $this->_sendJSONResponse(200,  json_encode(array('message'=>"foo")));
-			$this->_sendJSONResponse(400, json_encode(array('errors'=>array(
-				'card'=>'qué coño has pesto aquí?',
-			))));
-		}
 
-		$_SESSION['token'] = uniqid();
-		$this->render('register', ['token'=>$_SESSION['token']]);
+		$this->render('register');
+	}
+	
+	/**
+	 * Receive data via POST, create a new user with it and return a JSON response with the result.
+	 * @param  array $params Data received via POST
+	 * @return void         
+	 */
+	public function actionCreate($params) {
+		$user = new User($_POST);
+		try{
+			$result = $user->save();
+        	
+        	if(!$result->hasErrors()) {
+				$this->_sendJSONResponse(200,  json_encode(array('result'=>$result)));
+			} 
+			$errors = $result->getErrors();
+			$this->_sendJSONResponse(200,  json_encode(array('errors'=>$errors)));
+		}catch(PDOException $e) {
+			$this->_sendJSONResponse(200,  json_encode(array('dbError'=>$e->getMessage())));
+		}
 	}
 }
